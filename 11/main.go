fe9f76e5b91a7cd5420992a729f376ded36d8318
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-07 23:09:38 +0300

    refactor code

--- 11/main.go
+++ 11/main.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	set1 := []int{1, 7, 3, 5, 3}
-	set2 := []int{3, 6, 1, 9, 3, 7, 2, 4}
+	set1 := []int{1, 3, 12, 3, 5, 34, 77}
+	set2 := []int{3, 6, 12, 9, 3, 34, 2, 4, 77}
+	resultMap := make(map[int]bool)
 	result := []int{}
-
 	for _, val1 := range set1 {
 		for _, val2 := range set2 {
 			if val1 == val2 {
-				result = append(result, val1)
+				resultMap[val1] = true
 			}
+		}
+	}
+	for key, value := range resultMap {
+		if value {
+			result = append(result, key)
 		}
 	}
 	fmt.Println(result)

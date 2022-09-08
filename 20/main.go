e8297e9254310754b9daa74e61d1dc0c474e59e9
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-08 15:15:45 +0300

    add 20

--- 20/main.go
+++ 20/main.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 //Разработать программу, которая переворачивает слова в строке.
 //Пример: «snow dog sun — sun dog snow».
 
+func reverse(sw string) string {
+	words := strings.Split(sw, " ")
+	var rW []string
+	for i := len(words) - 1; i >= 0; i-- {
+		rW = append(rW, words[i])
+	}
+	var result string
+	for _, word := range rW {
+		result += word + " "
+	}
+	return result
+}
 func main() {
-
+	sw := "snow dog sun"
+	newString := reverse(sw)
+	fmt.Println(newString)
 }

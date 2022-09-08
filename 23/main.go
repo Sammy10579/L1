ddac167e4995b64d862b5f4c2a10a25c8a5ba2b9
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-08 16:06:40 +0300

    add 23

--- 23/main.go
+++ 23/main.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
 //Удалить i-ый элемент из слайса.
 
 func main() {
+	a := []string{"A", "B", "C", "D", "E"}
+	i := 2
 
+	copy(a[i:], a[i+1:])
+
+	a[len(a)-1] = ""
+
+	a = a[:len(a)-1]
+
+	fmt.Println(a)
 }

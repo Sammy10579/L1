4ea57671ec008c68bf5dc9dc60851afb32c96a7c
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-08 14:55:38 +0300

    add 19

--- 19/main.go
+++ 19/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func reverse(sw string) string {
+	sR := []rune(sw)
+	for a, b := 0, len(sw)-1; a < b; a, b = a+1, b-1 {
+		sR[a], sR[b] = sR[b], sR[a]
+	}
+	return string(sR)
+}
+func main() {
+	sw := "glavryba"
+	newString := reverse(sw)
+	fmt.Println(newString)
 }

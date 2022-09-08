b63c5246f636f53ea74ff1fcbbc12b63804e0eff
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-08 15:44:13 +0300

    add 21

--- 21/main.go
+++ 21/main.go
@@ -4,4 +4,30 @@ package main
 
 func main() {
 
+	client := &Client{}
+	mac := &Mac{}
+
+	client.InsertLightningConnectorIntoComputer(mac)
+
+	windowsMachine := &Windows{}
+	windowsMachineAdapter := &WindowsAdapter{
+		windowMachine: windowsMachine,
+	}
+
+	client.InsertLightningConnectorIntoComputer(windowsMachineAdapter)
 }
+
+/*type kilograms int
+
+func toGrams(g int) kilograms {
+	return kilograms(g * 1000)
+}
+
+func kilo(gr kilograms) {
+	fmt.Printf("grams - %d", gr)
+}
+
+func main() {
+	kg := 3
+	kilo(toGrams(kg))
+}*/

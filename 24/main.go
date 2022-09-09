bb1a6494a9042143b7d6a5e908b58711a1915c3c
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-09 21:05:42 +0300

    add 24 and 25

--- 24/main.go
+++ 24/main.go
@@ -1,9 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 //Разработать программу нахождения расстояния между двумя точками, которые
 //представлены в виде структуры Point с инкапсулированными параметрами x,y и
 //конструктором.
 
-func main() {
+type Point struct {
+	x int
+	y int
+}
 
+func NewPoint(x int, y int) Point {
+	return Point{x, y}
+}
+
+func Distance(point1 Point, point2 Point) float64 {
+	dx := point1.x - point2.x
+	dy := point1.y - point2.y
+	return math.Sqrt(float64(dx*dx + dy*dy))
+}
+
+func main() {
+	point1 := NewPoint(10, 12)
+	point2 := NewPoint(4, 5)
+	fmt.Printf("distance between %v and %v is - %f", point1, point2, Distance(point1, point2))
 }
--- 25/main.go
+++ 25/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 //Реализовать собственную функцию sleep.
 
+func sleep1(s time.Duration) {
+	<-time.After(s)
+}
+
+func sleep2(s time.Duration) {
+	<-time.NewTimer(s).C
+}
+
 func main() {
+	sleep1(time.Second * 3)
+	fmt.Println("it's sleep1")
+	sleep2(time.Second * 3)
+	fmt.Println("it's sleep2")
 
 }

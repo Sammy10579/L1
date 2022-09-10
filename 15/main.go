fb6d532443e797469f9578a9f511b5fb2cea6a07
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-10 19:12:34 +0300

    add 15

--- 15/main.go
+++ 15/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 //К каким негативным последствиям может привести данный фрагмент кода, и как
 //это исправить? Приведите корректный пример реализации.
 //var justString string
@@ -11,6 +16,22 @@ package main
 //someFunc()
 //}
 
-func main() {
+// глобальная переменная = доступ из любого места программы
+// если при создании строки использовать символы unicode,
+// то строка скопируется некорректно, необходимо добавить преобразование в тип rune
 
+func createHugeString(num int) string {
+	return strings.Repeat("o", num)
+}
+
+func someFunc() {
+
+	v := createHugeString(1 << 10) // 10 000 000 000
+	r := []rune(v)
+	str := string(r[:100])
+	fmt.Println(str)
+}
+
+func main() {
+	someFunc()
 }

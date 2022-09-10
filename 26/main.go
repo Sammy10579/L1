23e28c5505d5ac1798ef6e028643780763214674
Sammy10579 <ramenskiivlad@gmail.com>
2022-09-10 18:05:34 +0300

    add 26

--- 26/main.go
+++ 26/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 //Разработать программу, которая проверяет, что все символы в строке
 //уникальные (true — если уникальные, false etc). Функция проверки должна быть
 //регистронезависимой.
@@ -9,5 +14,20 @@ package main
 //aabcd — false
 
 func main() {
+	fmt.Println(CountLetters("abcde"))
+	fmt.Println(CountLetters("abcdee"))
+	fmt.Println(CountLetters("abCDe"))
+	fmt.Println(CountLetters("AbcDEe"))
+}
 
+func CountLetters(str string) (string, bool) {
+	s := strings.ToLower(str)
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				return s, false
+			}
+		}
+	}
+	return s, true
 }
